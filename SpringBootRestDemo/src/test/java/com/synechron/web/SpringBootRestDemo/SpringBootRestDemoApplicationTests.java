59353bcc20159367756bfed8e737df339ab5a899
Shalini Mittal <shalini06.mittal@gmail.com>
2024-07-15 11:22:48 +0530

    first commit

---
+++ SpringBootRestDemo/src/test/java/com/synechron/web/SpringBootRestDemo/SpringBootRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synechron.web.SpringBootRestDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
